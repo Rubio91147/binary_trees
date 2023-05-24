de4a08436bf395440f2ea3c1eea5e8cb40bb1485
Rubio91147 <124604809+Rubio91147@users.noreply.github.com>
2023-05-23 21:29:07 -0500

    Update 6-binary_tree_preorder.c

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,15 +12,15 @@
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL) {
-        return; // If tree or func is NULL, do nothing
+        return; /* If tree or func is NULL, do nothing */
     }
 
-    // Call the function for the current node
+    /* Call the function for the current node */
     func(tree->n);
 
-    // Traverse the left subtree recursively
+    /* Traverse the left subtree recursively */
     binary_tree_preorder(tree->left, func);
 
-    // Traverse the right subtree recursively
+    /* Traverse the right subtree recursively */
     binary_tree_preorder(tree->right, func);
 }
